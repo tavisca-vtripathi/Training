c37c734cc100e92d5e21fe6962ff859627689687
vtripathi <vtripathi@tavisca.com>
2015-07-07 13:38:20 +0530

    Operator Overloading For Review

---
+++ 01 Operator Overloading/Operator Overloading.Host/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OperatorOverloading.Model;
+
+namespace OperatorOverloading.Host
+{
+    public class Program                                                                              //Main Program
+    {
+        static void Main(string[] args)
+        {
+            try                                                                                     // try block to check validation on input string and currency match
+            {
+                Console.WriteLine("Enter the amount");
+                int amount = Convert.ToInt32(Console.ReadLine());                                  
+                string currency = Console.ReadLine();
+                Money firstMoneyObject = new Money(amount, currency);
+                Console.WriteLine("Enter the amount :");
+                int amount1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter Currency");
+                string currency2 = Console.ReadLine();
+                Money secondMoneyObject = new Money(amount1, currency2);
+
+                Money sumMoneyObject = firstMoneyObject + secondMoneyObject;                        //Operator Overloading
+                Console.WriteLine("Sum is : "+sumMoneyObject.Amount+" " + sumMoneyObject.Currency);
+            }
+            
+            catch (ArgumentException e)                                                         // Catching exception for integer value
+            {
+                Console.WriteLine("Amount entered is less than zero");
+            
+            }
+            catch (Exception ex)                                                              // Catching exception for currency mismatch
+            {
+                Console.WriteLine("OOPS !! There is some error");
+
+
+            }
+
+
+            Console.ReadLine();
+
+
+
+
+
+        }
+    }
+}
---
+++ 01 Operator Overloading/Operator Overloading.Model/Money.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorOverloading.Model
+{
+    public class Money                                                           
+    {
+        public int Amount;
+        public string Currency;
+        
+        public Money()
+        { }                                                           
+
+        public Money(int amt, string curr)                                                                        //Takes input                                                         
+        {
+            if (amt < 0)
+            {
+                throw new ArgumentException();                                                                     ///throw exception for Integer out of range
+            }
+            else
+            {
+                this.Amount = amt;
+                this.Currency = curr;
+            }
+
+        }
+
+        public static Money operator +(Money money1, Money money2)                                                  //operator overloading function    
+        { 
+            Money money3 = new Money();
+
+                if (money1.Currency == money2.Currency)
+                {
+
+
+                    money3.Amount = money1.Amount + money2.Amount;
+                    money3.Currency = money1.Currency;
+                }
+                else
+                {
+                    throw new System.Exception();                                                                     //throw exception for currency mismatch
+                }
+
+            
+            return money3;
+        }
+    }
+}
