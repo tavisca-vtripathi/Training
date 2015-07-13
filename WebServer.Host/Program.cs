98e5b6615773705baa7119e1e4471defddf7d2ba
vtripathi <vtripathi@tavisca.com>
2015-07-13 20:33:07 +0530

    First Commit for Web Server

---
+++ WebServer.Host/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebServer.Model;
+
+namespace WebServer.Model
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Listener listener = new Listener(8080);
+                listener.Start();
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ WebServer.Model/Dispatcher.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Threading;
+using System.Configuration;
+
+namespace WebServer.Model
+{
+    class Dispatcher
+    {
+        private Socket _clientSocket = null; // use a queue later on
+
+        public Dispatcher(Socket clientSocket)
+        {
+            _clientSocket = clientSocket;
+        }
+        public void HandleClient()
+        {
+            var requestParser = new RequestParser();
+            string requestString = DecodeRequest(_clientSocket);
+            requestParser.Parser(requestString);
+
+            Console.WriteLine(requestParser.HttpUrl);
+
+            if (requestParser.HttpMethod != null && requestParser.HttpMethod.Equals("GET"))
+            {
+                //TO DO: Select appropriate class for serving the received GET request.....
+                var createResponse = new CreateResponse(_clientSocket, ConfigurationManager.AppSettings["Path"]);
+                createResponse.RequestUrl(requestParser.HttpUrl);
+            }
+            else
+            {
+                Thread.Yield();
+
+            }
+            StopClientSocket(_clientSocket);  //closes the connection
+        }
+
+        public void StopClientSocket(Socket clientSocket)
+        {
+            if (clientSocket != null)
+                clientSocket.Close();
+        }
+
+        private string DecodeRequest(Socket clientSocket)
+        {
+            Encoding _charEncoder = Encoding.UTF8;
+            var receivedBufferlen = 0;
+            var buffer = new byte[10240];
+            try
+            {
+                receivedBufferlen = clientSocket.Receive(buffer);
+            }
+            catch (Exception)
+            {
+
+                Thread.Yield();
+            }
+            return _charEncoder.GetString(buffer, 0, receivedBufferlen);
+        }
+
+
+
+    }
+}
