8bdb5d09f9be4c625434acc930005f8f0b9fde50
vtripathi <vtripathi@tavisca.com>
2015-07-20 21:53:01 +0530

    Bouncing ball commit with View

---
+++ BouncingBall/View/ball.js
@@ -0,0 +1,19 @@
+﻿window.ball = window.ball || {
+    axisX: 0,
+    axisY: 0,
+    dx: 10,
+    dy: 10
+};
+
+ball.bounce = function () {
+    var element = document.getElementById('circle');
+    element.style.background = "black";
+    element.style.left = ball.axisX + "px";
+    element.style.top = ball.axisY + "px";
+    ball.container();
+    ball.axisX += ball.dx;
+    ball.axisY += ball.dy;
+}
+window.ball.start = function () {
+    setInterval(ball.bounce, 10);
+}
---
+++ BouncingBall/View/container.js
@@ -0,0 +1,20 @@
+﻿window.ball = window.ball || {}
+
+ball.container = function () {
+
+    var height = (window.innerHeight - 50);
+    var width = (window.innerWidth - 50);
+    if (ball.axisY >= height) {
+        ball.dy = -10;
+    }
+    if (ball.axisX >= width) {
+        ball.dx = -10;
+    }
+    if (ball.axisY <= 0) {
+        ball.dy = 10;
+    }
+    if (ball.axisX <= 0) {
+        ball.dx = 10;
+    }
+}
+
